dcad7381d039e7c75d3c1abb6be4773bd0834bcf
Rodrigo Teobaldo <rodrigo@etools.com.br>
2014-09-04 19:37:51 -0300

    Added method to return selected option.

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
-simple-select
-====================
+# Simple Select
 
 Simple select to replace `<select>` when need styling it.
 
 Inspired on @blakehaswell gist: https://gist.github.com/blakehaswell/2424807
-
-
-## TODO
-- Use `document.createDocumentFragment` instead `document.createElement`;
-- Support to `<optgroup>`;
-- Support to `<option value="">` (Currently only `innerHTML` of `<option>` is considered.);
--- dist/simple-select.js
+++ dist/simple-select.js
@@ -13,12 +13,13 @@
     _simpleSelect: {},
     _optContainer: {},
     _itemSelected: {},
+    _itemSelectedValue: {},
+    _values: [],
 
     _init: function (el) {
       /**
        * Elements configuration
        */
-      var docFrag = document.createDocumentFragment();
 
       var oldSelect = document.querySelector(el);
       var oldSelectOptions = oldSelect.querySelectorAll('option');
@@ -36,17 +37,28 @@
       for (var i = 0; i < oldSelectOptions.length; i++) {
         var li = document.createElement('li');
         li.textContent = oldSelectOptions[i].textContent;
-        li.value = oldSelectOptions[i].innerHTML;
+        li.value = i;
         options.appendChild(li);
+        this._values.push(oldSelectOptions[i].value);
       }
 
       this._button = document.createElement('button');
       this._button.className = 'SimpleSelect-button';
-      this._button.type = 'button';
 
       this._itemSelected = document.createElement('span');
       this._itemSelected.className = 'SimpleSelect-itemSelected';
       this._itemSelected.innerHTML = options.getElementsByTagName('li')[0].innerHTML;
+
+      this._itemSelectedValue = document.createElement('input');
+      this._itemSelectedValue.className = 'SimpleSelect-itemSelectedValue';
+      this._itemSelectedValue.type = 'hidden';
+      this._itemSelectedValue.value = options.getElementsByTagName('li')[0].value;
+      if (oldSelect.hasAttribute('name')) {
+        this._itemSelectedValue.setAttribute( 'name', oldSelect.getAttribute('name') );
+      }
+      if (oldSelect.hasAttribute('form')) {
+        this._itemSelectedValue.setAttribute( 'form', oldSelect.getAttribute('form') );
+      }
 
       var caret = document.createElement('span');
       caret.className = 'SimpleSelect-caret';
@@ -56,8 +68,9 @@
        * Join all elements (Megazord?!)
        * (Don't change order of appendChild's!)
        */
-      //docFrag.appendChild(this._button);
+
       this._button.appendChild(this._itemSelected);
+      this._button.appendChild(this._itemSelectedValue);
       this._button.appendChild(caret);
       this._simpleSelect.appendChild(this._button);
       this._optContainer.appendChild(options);
@@ -67,44 +80,79 @@
       oldSelect.parentNode.replaceChild(this._simpleSelect, oldSelect);
     },
 
+    // Returns a object with value and text of selected option
+    getSelected: function () {
+      return { value: this._itemSelectedValue.value, text: this._itemSelected.innerHTML };
+    },
+
     /**
      * Add Event Listners
      */
     _initEventHandlers: function () {
 
-      var self = this;
-      var showSelectOptionsHandler = function () {
-        if ( self._hasClass(self._simpleSelect, 'is-open') )
-        {
-          self._removeClass(self._simpleSelect, 'is-open');
-          self._optContainer.style.display = '';
+      var _this = this;
+
+      var showSelectOptionsHandler = function (open) {
+
+        var _show = function () {
+          _this._addClass(_this._simpleSelect, 'is-open');
+          _this._optContainer.style.display = 'block';
+        };
+
+        var _close = function () {
+          _this._removeClass(_this._simpleSelect, 'is-open');
+          _this._optContainer.style.display = '';
+        };
+
+        if ( _this._hasClass(_this._simpleSelect, 'is-open') ) {
+          _close();
+        } else {
+          _show();
         }
-        else
-        {
-          self._addClass(self._simpleSelect, 'is-open');
-          self._optContainer.style.display = 'block';
+
+        if (open === false) {
+          _close();
+        } else if (open === true) {
+          _open();
         }
+
       };
 
       var itemSelectHandler = function (event) {
-        if (event.target.tagName === 'LI')
-        {
-          var itemSelectedExists = self._optContainer.querySelector('.is-selected');
-          itemSelectedExists && self._removeClass(itemSelectedExists, 'is-selected');
+        if (event.target.tagName === 'LI') {
+          var itemSelectedExists = _this._optContainer.querySelector('.is-selected');
+          itemSelectedExists && _this._removeClass(itemSelectedExists, 'is-selected');
 
           var target = event.target;
           target.className = 'is-selected';
-          self._itemSelected.innerHTML = target.innerHTML;
+          _this._itemSelected.innerHTML = target.innerHTML;
+          _this._itemSelectedValue.value = _this._values[target.value];
           showSelectOptionsHandler();
         }
       };
 
-      self._button.addEventListener('click', function () {
+      // IE8 Hack
+      if (typeof Element.prototype.addEventListener === 'undefined') {
+        Element.prototype.addEventListener = function (e, callback) {
+          e = 'on' + e;
+          return this.attachEvent(e, callback);
+        };
+      }
+
+      _this._button.addEventListener('click', function (event) {
         showSelectOptionsHandler();
+        event.stopPropagation();
+        return false;
       });
 
-      self._optContainer.querySelector('.SimpleSelect-options').addEventListener('click', function (event) {
+      _this._optContainer
+      .querySelector('.SimpleSelect-options')
+      .addEventListener('click', function (event) {
         itemSelectHandler(event);
+      });
+
+      document.addEventListener('click', function () {
+        showSelectOptionsHandler(false);
       });
 
     },
@@ -125,7 +173,9 @@
 
     _removeClass: function (el, name) {
       if (this._hasClass(el, name)) {
-        el.className = el.className.replace(new RegExp('(\\s|^)' + name + '(\\s|$)'), ' ').replace(/^\s+|\s+$/g, '');
+        el.className = el.className
+                         .replace(new RegExp('(\\s|^)' + name + '(\\s|$)'), ' ')
+                         .replace(/^\s+|\s+$/g, '');
       }
     }
 
