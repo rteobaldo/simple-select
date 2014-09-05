12bdb2c86075c1ccfe7499944b5402bbf9d5872e
Rodrigo Teobaldo <rodrigo@etools.com.br>
2014-09-05 17:32:47 -0300

    Now working on IE8 :D

--- dist/simple-select.js
+++ dist/simple-select.js
@@ -1,8 +1,8 @@
-(function () {
+(function() {
 
-  "use strict";
+  'use strict';
 
-  window.SimpleSelect = function (el) {
+  window.SimpleSelect = function(el) {
     this._init(el);
     this._initEventHandlers();
   };
@@ -16,13 +16,19 @@
     _itemSelectedValue: {},
     _values: [],
 
-    _init: function (el) {
-      /**
-       * Elements configuration
-       */
+    _init: function(el) {
 
-      var oldSelect = document.querySelector(el);
-      var oldSelectOptions = oldSelect.querySelectorAll('option');
+      /**
+      * Elements configuration
+      */
+
+      var oldSelect,
+          oldSelectOptions,
+          options,
+          caret;
+
+      oldSelect = document.querySelector(el);
+      oldSelectOptions = oldSelect.querySelectorAll('option');
 
       this._simpleSelect = document.createElement('div');
       this._simpleSelect.className = 'SimpleSelect' + ' ' + oldSelect.className;
@@ -30,13 +36,13 @@
       this._optContainer = document.createElement('div');
       this._optContainer.className = 'SimpleSelect-optContainer';
 
-      var options = document.createElement('ul');
+      options = document.createElement('ul');
       options.className = 'SimpleSelect-options';
 
       // Copy <option>'s from oldSelect to new dropdown element
       for (var i = 0; i < oldSelectOptions.length; i++) {
         var li = document.createElement('li');
-        li.textContent = oldSelectOptions[i].textContent;
+        li.innerHTML = oldSelectOptions[i].innerHTML;
         li.value = i;
         options.appendChild(li);
         this._values.push(oldSelectOptions[i].value);
@@ -53,22 +59,19 @@
       this._itemSelectedValue.className = 'SimpleSelect-itemSelectedValue';
       this._itemSelectedValue.type = 'hidden';
       this._itemSelectedValue.value = options.getElementsByTagName('li')[0].value;
+
+      // Adds attributes from oldSelect
       if (oldSelect.hasAttribute('name')) {
-        this._itemSelectedValue.setAttribute( 'name', oldSelect.getAttribute('name') );
+        this._itemSelectedValue.setAttribute('name', oldSelect.getAttribute('name'));
       }
       if (oldSelect.hasAttribute('form')) {
-        this._itemSelectedValue.setAttribute( 'form', oldSelect.getAttribute('form') );
+        this._itemSelectedValue.setAttribute('form', oldSelect.getAttribute('form'));
       }
 
-      var caret = document.createElement('span');
+      caret = document.createElement('span');
       caret.className = 'SimpleSelect-caret';
 
-
-      /**
-       * Join all elements (Megazord?!)
-       * (Don't change order of appendChild's!)
-       */
-
+      // Join all elements
       this._button.appendChild(this._itemSelected);
       this._button.appendChild(this._itemSelectedValue);
       this._button.appendChild(caret);
@@ -81,49 +84,57 @@
     },
 
     // Returns a object with value and text of selected option
-    getSelected: function () {
-      return { value: this._itemSelectedValue.value, text: this._itemSelected.innerHTML };
+    getSelected: function() {
+      return window.asd = { value: this._itemSelectedValue.value, text: this._itemSelected.innerHTML };
     },
 
     /**
-     * Add Event Listners
-     */
-    _initEventHandlers: function () {
+    * Add Event Listners
+    */
+    _initEventHandlers: function() {
 
-      var _this = this;
+      var showSelectOptionsHandler,
+          itemSelectHandler,
+          _show,
+          _close,
+          _this = this;
 
-      var showSelectOptionsHandler = function (open) {
+      showSelectOptionsHandler = function(open) {
 
-        var _show = function () {
+        _show = function() {
           _this._addClass(_this._simpleSelect, 'is-open');
           _this._optContainer.style.display = 'block';
         };
 
-        var _close = function () {
+        _close = function() {
           _this._removeClass(_this._simpleSelect, 'is-open');
           _this._optContainer.style.display = '';
         };
 
-        if ( _this._hasClass(_this._simpleSelect, 'is-open') ) {
-          _close();
+        if (typeof open === 'undefined') {
+          if (_this._hasClass(_this._simpleSelect, 'is-open')) {
+            _close();
+          } else {
+            _show();
+          }
         } else {
-          _show();
-        }
-
-        if (open === false) {
-          _close();
-        } else if (open === true) {
-          _open();
+          if (open) {
+            _open();
+          } else {
+            _close();
+          }
         }
 
       };
 
-      var itemSelectHandler = function (event) {
-        if (event.target.tagName === 'LI') {
-          var itemSelectedExists = _this._optContainer.querySelector('.is-selected');
-          itemSelectedExists && _this._removeClass(itemSelectedExists, 'is-selected');
+      itemSelectHandler = function(event) {
+        var itemSelectedExists,
+            target = event.target || event.srcElement;
 
-          var target = event.target;
+        if (target.nodeName === 'LI') {
+
+          itemSelectedExists = _this._optContainer.querySelector('.is-selected');
+          itemSelectedExists && _this._removeClass(itemSelectedExists, 'is-selected');
           target.className = 'is-selected';
           _this._itemSelected.innerHTML = target.innerHTML;
           _this._itemSelectedValue.value = _this._values[target.value];
@@ -131,47 +142,39 @@
         }
       };
 
-      // IE8 Hack
-      if (typeof Element.prototype.addEventListener === 'undefined') {
-        Element.prototype.addEventListener = function (e, callback) {
-          e = 'on' + e;
-          return this.attachEvent(e, callback);
-        };
-      }
-
-      _this._button.addEventListener('click', function (event) {
+      _this._button.addEventListener('click', function(event) {
         showSelectOptionsHandler();
         event.stopPropagation();
-        return false;
       });
 
       _this._optContainer
       .querySelector('.SimpleSelect-options')
-      .addEventListener('click', function (event) {
+      .addEventListener('click', function(event) {
         itemSelectHandler(event);
+        event.stopPropagation();
       });
 
-      document.addEventListener('click', function () {
+      document.addEventListener('click', function() {
         showSelectOptionsHandler(false);
       });
 
     },
 
     /**
-     * Class manipulation functions stolen from Arjan Haverkamp (av01d).
-     * http://www.avoid.org/?p=78
-     */
-    _hasClass: function (el, name) {
+    * Class manipulation functions stolen from Arjan Haverkamp (av01d).
+    * http://www.avoid.org/?p=78
+    */
+    _hasClass: function(el, name) {
       return new RegExp('(\\s|^)' + name + '(\\s|$)').test(el.className);
     },
 
-    _addClass: function (el, name) {
+    _addClass: function(el, name) {
       if (!this._hasClass(el, name)) {
         el.className += (el.className ? ' ' : '') + name;
       }
     },
 
-    _removeClass: function (el, name) {
+    _removeClass: function(el, name) {
       if (this._hasClass(el, name)) {
         el.className = el.className
                          .replace(new RegExp('(\\s|^)' + name + '(\\s|$)'), ' ')
@@ -179,8 +182,25 @@
       }
     }
 
-
   };
 
+  /**
+   * IE8 Shims
+   */
+  if (typeof Element.prototype.addEventListener === 'undefined') {
+    var addEventListener = function(e, callback) {
+      e = 'on' + e;
+      return this.attachEvent(e, callback);
+    };
+
+    HTMLDocument.prototype.addEventListener = addEventListener;
+    Element.prototype.addEventListener = addEventListener;
+  }
+  if (typeof Event.prototype.stopPropagation === 'undefined') {
+    Event.prototype.stopPropagation = function() {
+      this.returnFalse = false;
+      this.cancelBubble = true;
+    };
+  }
 
 })();
